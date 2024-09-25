7e8d0d7617133c35e453d83cf55e88ecdf119203
Owen Bean <owenbean@Owens-MacBook-Pro-98.local>
2024-09-25 09:23:34 -0400

    Make separate page for forgot password

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ForgotPassword from './pages/ForgotPassword';
+import Main from './pages/Main';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ForgotPassword />} />
+        <Route path="/" element={<Main />} exact />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Main.js
@@ -0,0 +1,9 @@
+function Main() {
+    return (
+        <div>
+            <h1>In Construction</h1>
+        </div>
+    )
+}
+
+export default Main;
